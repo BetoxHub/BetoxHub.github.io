aa5a637d3e7d373b1013f2519b713b325f257aaf
BetoxHub <123609731+BetoxHub@users.noreply.github.com>
2025-01-21 14:18:24 -0500

    Add files via upload

---
+++ volumen.js
@@ -0,0 +1,11 @@
+const audio = document.getElementById("myAudio");
+const adjustButton = document.getElementById("adjustVolume");
+
+// Inicializar el volumen
+audio.volume = 0;
+
+// Mostrar el control de volumen al hacer clic en el botón
+adjustButton.addEventListener('click', () => {
+  // Aquí puedes mostrar un elemento con un control de rango para ajustar el volumen
+  // Por ejemplo, utilizando una librería como jQuery o creando un elemento HTML dinámicamente
+});
